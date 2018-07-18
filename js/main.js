e466b5be315ad2e8c572ba6cfcd9d3671cfd410c
qinggge <zhuhuang@126.com>
2018-07-18 18:08:13 +0800

    make-a-pikachu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 ! function () {
+    let duration = 50
+    $('.actions').on('click','button',function(e){
+        let $button = $(e.currentTarget) // button
+        let speed = $button.attr('data-speed')
+        console.log(speed)
+        $button.addClass('active')
+            .siblings('.active').removeClass('active')
+        switch(speed){
+            case 'slow':
+                duration = 100
+                break
+            case 'normal':
+                duration = 50
+                break
+            case 'fast':
+                duration = 10
+                break
+        }
+    })
     function writeCode(prefix, code, fn) {
         let container = document.querySelector('#code')
         let styleTag = document.querySelector('#styleTag')
         let n = 0
-        var id = setInterval(() => {
+        let id
+        id = setTimeout(function run(){
             n += 1
             container.innerHTML = code.substring(0, n)
             styleTag.innerHTML = code.substring(0, n)
             container.scrollTop = container.scrollHeight
-            if (n >= code.length) {
-                window.clearInterval(id)
+            if (n < code.length) {
+               id = setTimeout(run,duration)
+            }else{
+                fn && fn.call()
             }
-        }, 30)
+        }, duration)
     }
 
     let code = `
@@ -179,6 +201,9 @@
         margin-left: -50px;
         border-radius: 50px;
       }
+      /*
+       * 好了，这只皮卡丘送给你
+       */
     `
     writeCode('', code)
 }.call()
